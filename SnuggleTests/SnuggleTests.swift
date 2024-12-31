d7992d3203cf56eca101b2a9479479316fd5d9ec
89Explorer <leral3@naver.com>
2024-12-31 16:14:12 +0900

    Initial Commit

---
+++ SnuggleTests/SnuggleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnuggleTests.swift
+//  SnuggleTests
+//
+//  Created by 권정근 on 12/31/24.
+//
+
+import Testing
+@testable import Snuggle
+
+struct SnuggleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
